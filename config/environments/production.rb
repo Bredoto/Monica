1b2bfab3de995aad3dddc7f3e5a91f5f2784ca82
Bredoto <serg.rumyantsev@gmail.com>
2021-10-19 14:04:35 +0300

    add config into environment production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+
+
+
+  config.active_record.encryption.key_provider = ActiveRecord::Encryption::EnvelopeEncryptionKeyProvider.new
+  config.active_record.encryption.store_key_references = true
+  config.active_record.encryption.extend_queries = true
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
